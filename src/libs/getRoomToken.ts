7de446960f34c02a834f9b6727328a051ae03403
空雲 <info@croud.jp>
2021-02-05 13:58:29 +0900

    認証系修正

--- src/libs/getRoomToken.ts
+++ src/libs/getRoomToken.ts
@@ -1,4 +1,4 @@
 export const getRoomToken = (room: string, name: string) =>
   fetch(`/api/auth?room=${room}&name=${name}`)
     .then((res) => res.json())
-    .then((value) => value.token);
+    .then((value) => (value.result === 'OK' ? value.token : null));
--- src/pages/api/auth.tsx
+++ src/pages/api/auth.tsx
@@ -6,17 +6,19 @@ const { ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET } = process.env;
 const index: NextApiHandler = (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-
   const { name, room } = req.query;
-
-  const accessToken = new jwt.AccessToken(ACCOUNT_SID!, API_KEY_SID!, API_KEY_SECRET!, {
-    identity: typeof name === 'string' ? name : '',
-  });
-  const grant = new jwt.AccessToken.VideoGrant({
-    room: typeof room === 'string' ? room : '',
-  });
-  accessToken.addGrant(grant);
-  const token = accessToken.toJwt();
-  res.end(JSON.stringify({ token }));
+  if (ACCOUNT_SID && API_KEY_SID && API_KEY_SECRET) {
+    const accessToken = new jwt.AccessToken(ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET, {
+      identity: typeof name === 'string' ? name : '',
+    });
+    const grant = new jwt.AccessToken.VideoGrant({
+      room: typeof room === 'string' ? room : '',
+    });
+    accessToken.addGrant(grant);
+    const token = accessToken.toJwt();
+    res.end(JSON.stringify({ result: 'OK', token }));
+  } else {
+    res.end(JSON.stringify({ result: 'NG' }));
+  }
 };
 export default index;
